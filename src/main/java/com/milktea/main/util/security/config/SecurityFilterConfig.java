06cf6382fa8549556296d0ed850fb20cd3b65743
MilkTea24 <kimmanjae15@pusan.ac.kr>
2024-01-15 17:11:36 +0900

    fix: AuthenticationManager에 AuthenticationProvider 연결
     - AuthenticationProvider를 연결하고 AuthenticationManager 빈을 AuthenticationManagerConfig로 분리
     - 모든 SecurityConfig 파일들을 config 패키지로 이동

--- src/main/java/com/milktea/main/util/security/config/SecurityFilterConfig.java
+++ src/main/java/com/milktea/main/util/security/config/SecurityFilterConfig.java
@@ -1,10 +1,15 @@
-package com.milktea.main.util.security;
+package com.milktea.main.util.security.config;
 
+import com.milktea.main.util.security.InitialAuthenticationFilter;
+import com.milktea.main.util.security.JwtAuthenticationFilter;
+import com.milktea.main.util.security.UsernamePasswordAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
@@ -42,6 +47,4 @@ public class SecurityFilterConfig {
 
         return http.build();
     }
-
-
 }
