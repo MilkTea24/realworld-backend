d9962bc3c1150bf12eaa9c54b66094baa24ce03d
MilkTea24 <kimmanjae15@pusan.ac.kr>
2024-01-29 13:38:50 +0900

    fix: LazyInitializationException 해결

--- src/main/java/com/milktea/main/user/repository/UserRepository.java
+++ src/main/java/com/milktea/main/user/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.milktea.main.user.repository;
 
 import com.milktea.main.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"authorities"})
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUsername(String username);
